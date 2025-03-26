92da3d1a9946d3a28ffaf9ccf5b149204c0b4f24
Lucas Goshi <lucascgoshi04@gmail.com>
2025-03-26 18:41:03 -0300

    teste com teste.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,11 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('soma 2 + 2 deve ser igual a 4', () => {
+  expect(2 + 2).toBe(4);
+});
+
+test('soma 2 + 2 deve ser igual a 5 (erro proposital)', () => {
+  expect(2 + 2).toBe(5);
+});
